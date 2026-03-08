5b792b91dc03a5c6704aedaad1d1d3f2b405b7d1
h4llstar <jadenblackerson@gmail.com>
2026-03-08 17:29:44 +0000

    Update script URLs for game loading

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
 repeat task.wait() until game:IsLoaded()
 if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId == 139566161526375 then
-	--loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
+	--loadstring(game:HttpGet("https://raw.githubusercontent.com/h4llstar/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
 elseif game.PlaceId == 71480482338212 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/sigeon.beta/refs/heads/main/bed_fight.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/h4llstar/sigeon.beta/refs/heads/main/bed_fight.lua"))()
 else
 	game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces")
 end
